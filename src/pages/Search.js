a7c7c02728ea59e0090ee1b795cf6867b0451436
Marshall Tuinier <mtuinier@gmail.com>
2018-05-24 11:26:42 -0700

    ADDED IDX MAP SEARCH

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 import UnderNavImg from '../components/UnderNavImg';
 
 const Search = () => {
   return (
     <div>
       <UnderNavImg />
-      <h1>TODO: Search Page</h1>
+      <StyledContainer>
+        <iframe
+          src="https://las.mlsmatrix.com/Matrix/public/IDX.aspx?idx=d07f59b"
+          width="100%"
+          height="800px"
+          frameborder="0"
+          marginwidth="0"
+          marginheight="0"
+        />
+      </StyledContainer>
     </div>
   );
 };
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  margin-bottom: -40px;
+`;
 export default Search;
