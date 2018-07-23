6b901cb938d1c321aa37f938f02eb5202cf22b47
Marshall Tuinier <mtuinier@gmail.com>
2018-07-23 13:12:21 -0700

    BUGFIX

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,15 @@ module.exports = {
     title: `Making Nevada Home`
   },
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     {
-  resolve: `gatsby-source-filesystem`,
-  options: {
-    path: `${__dirname}/src/assets`,
-    name: 'assets',
-  },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp` /*
-    {
-      resolve: `gatsby-plugin-typography`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`
-      },
-    },*/
+        path: `${__dirname}/src/assets`,
+        name: 'assets'
+      }
+    }
   ]
 };
