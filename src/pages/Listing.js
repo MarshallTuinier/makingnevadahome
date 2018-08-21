4f07399278a405cb5a5236e30ff86e92a2ecdd96
Marshall Tuinier <mtuinier@gmail.com>
2018-08-21 14:30:00 -0700

    removed query-string dependency

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import queryString from 'query-string';
 
 class Listing extends Component {
   render() {
-    const nmls = queryString.parse(this.props.location.search).nmls;
-    console.log(nmls);
+    const mls = this.props.location.search.slice(5, -10);
+    console.log(mls);
     return (
       <div style={{ height: '95vw', maxHeight: '1100px' }}>
         <iframe
-          src={`https://www.propertypanorama.com/instaview/las/${nmls}`}
+          src={`https://www.propertypanorama.com/instaview/las/${mls}`}
           width="99%"
           height="100%"
         />
