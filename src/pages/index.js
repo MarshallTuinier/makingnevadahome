1230cb18808f6cfe1c107f153aa8da62d998916b
Marshall Tuinier <mtuinier@gmail.com>
2018-08-29 12:13:36 -0700

    moved images to cloudinary

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ImageCarousel from '../components/ImageCarousel';
-import AgentInfo from '../components/AgentInfo';
-import Communities from '../components/Communities';
-import PreviousListings from '../components/PreviousListings';
-import Testimonials from '../components/Testimonials';
-import styled from 'styled-components';
+import React from "react";
+import ImageCarousel from "../components/ImageCarousel";
+import AgentInfo from "../components/AgentInfo";
+import Communities from "../components/Communities";
+import PreviousListings from "../components/PreviousListings";
+import Testimonials from "../components/Testimonials";
+import styled from "styled-components";
 
 const IndexPage = props => (
   <ResponsiveContainer>
@@ -23,33 +23,3 @@ const ResponsiveContainer = styled.div`
   flex-direction: column;
 `;
 export default IndexPage;
-
-export const PageQuery = graphql`
-  query SliderImageQuery {
-    sliderImg1: imageSharp(id: { regex: "/sliderImg1.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    sliderImg2: imageSharp(id: { regex: "/sliderImg2.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    sliderImg3: imageSharp(id: { regex: "/sliderImg3.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    sliderImg4: imageSharp(id: { regex: "/sliderImg4.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    sliderImg5: imageSharp(id: { regex: "/sliderImg5.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-  }
-`;
